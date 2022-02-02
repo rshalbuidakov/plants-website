99423a1869be1991ceacfefc82ee2415d1acda8b
rshalbuidakov <rshalbuidakov@gmail.com>
2022-02-03 03:05:44 +0600

    Scroll manipulations

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,6 @@ function scrollHeader() {
     header.classList.remove('scroll-header');
   }
 }
-
 window.addEventListener('scroll', scrollHeader);
 
 // QUESTIONS ACCORDION
@@ -64,3 +63,34 @@ accordionItems.forEach((item) => {
     }
   }
 })
+
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 58;
+    const sectionId = current.getAttribute('id');
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+    } else {
+      document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
+
+
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+
+  if (this.scrollY >= 200) {
+    scrollUp.classList.add('show-scroll');
+  } else {
+    scrollUp.classList.remove('show-scroll');
+  }
+}
+window.addEventListener('scroll', scrollUp);
